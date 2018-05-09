e0669d38d50084fa634abb71124044b4fb9e82d7
Ahmad Mohammad Abdullah <ahmedwahy@gmail.com>
2018-05-09 12:29:14 +0200

    Update cl.py

--- cl.py
+++ cl.py
@@ -2,14 +2,15 @@ import requests
 
 #http://docs.python-requests.org/en/latest/user/quickstart/#post-a-multipart-encoded-file
 
+file_name = '12-1-2.wav' # choose what ever file you want to be predicted
 post_url = "http://localhost:5000/"
-fin = open('12-114-2.wav', 'rb')
-get_url = "http://localhost:5000/uploads/" + '12-114-2.wav'
-print(get_url)
+fin = open(file_name, 'rb')
+get_url = "http://localhost:5000/uploads/" + file_name
+#print(get_url)
 files = {'file': fin}
 try:
   r = requests.post(post_url, files=files)
-  print (r.text)
+  #print (r.text)
   r = requests.get(get_url)
   print (r.text)
 finally:
